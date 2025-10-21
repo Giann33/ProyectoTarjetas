fcd302a49c33df142f3ad34cc1f365eb2e87c183
Giann <giann33@outlook.com>
2025-10-20 19:38:25 -0600

    corregir errores

--- backend/pagos/src/main/java/com/app/pagos/entity/Comercio.java
+++ backend/pagos/src/main/java/com/app/pagos/entity/Comercio.java
@@ -2,23 +2,26 @@ package com.app.pagos.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.time.Instant;
 
 @Entity
 @Table(name = "comercio")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class Comercio {
-  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @Column(nullable = false, length = 150)
   private String nombre;
 
+  @Builder.Default
   @Column(nullable = false, length = 30)
   private String estado = "ACTIVO";
 
+  @Builder.Default
   private Instant creadoEn = Instant.now();
+
   private Instant actualizadoEn;
   private Instant eliminadoEn;
 }
